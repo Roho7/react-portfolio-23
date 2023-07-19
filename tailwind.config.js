2ded266482aea4dfa26116c0ce4b5b8e48c2513e
Roho7 <rohosen2@gmail.com>
2023-07-19 15:19:57 +0530

    homepage built

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      display: "Darker Grotesque, sans-serif",
+      sans: "Montserrat, sans-serif",
+    },
+    extend: {
+      colors: {
+        grass: {
+          100: "#E7F69B",
+          500: "#AEC926",
+          800: "#556508",
+        },
+        white: "#FFFEFE",
+        ash: {
+          100: "#A9B9BD",
+          500: "#3A5359",
+          900: "#010C0F",
+        },
+        error: "#B62A2A",
+        success: "#2AA5C7",
+      },
+    },
+  },
+  plugins: [],
+};
